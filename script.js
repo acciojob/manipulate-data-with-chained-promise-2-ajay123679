bd018993ea894a5625f9d6d65687cc17acd2ca56
ajay123679 <65329197+ajay123679@users.noreply.github.com>
2025-05-10 15:28:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+const a=document.getElementById("output");
+const arr=[1,2,3,4];
+
+async function fun() {
+	let m = await new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			let ar=arr.filter((val)=>{
+				if(val%2==0)
+				{
+					return val;
+				}
+			})
+			a.innerText=ar;
+		    resolve(ar);
+		},1000);
+		
+	});
+	await new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			let arrr=m.map((val)=>{
+				return val*2;
+			})
+			a.innerText=arrr;
+			resolve();
+		},3000);
+		
+	})
+	
+}
+fun();
